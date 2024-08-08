a40b34cf149a9c588b69d4f511664f310a82855a
sumaiya <you@example.com>
2024-08-09 00:21:36 +0600

    Slider One Js Code Added

---
+++ script.js
@@ -0,0 +1,20 @@
+//For Slider One Starts
+$('.owl_carousel_slider_one').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navContainer:'#myNav',
+    navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+//For Slider One Ends
