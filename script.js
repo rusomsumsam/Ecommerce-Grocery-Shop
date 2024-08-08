31633a26c269b2fca68ced6157b43417735c0ccc
sumaiya <you@example.com>
2024-08-09 01:25:52 +0600

    Special Offers Slider and Responsive Completed

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ $('.owl_carousel_slider_one').owlCarousel({
     }
 })
 //For Slider One Ends
+//Special Offers Slider Starts
+$('.owl_carousel_slider_two').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    dots:false,
+    navContainer:'#myNavTwo',
+    navText:['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+//Special Offers Slider Ends
